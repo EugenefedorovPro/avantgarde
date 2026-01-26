d2799d077cd39f38fb64a84902838f70b3998299
eugene <eugene.proskulikov@gmail.com>
2026-01-26 10:17:54 +0200

    models + admins + initial

--- backend/avantgarde/admin.py
+++ backend/avantgarde/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import (
+    RawVerses,
+    EuPro,
+    Hermeneutics,
+    Audio,
+    Reclamation,
+    AnswerToReclamation,
+)
 
-# Register your models here.
+
+@admin.register(Reclamation)
+class ReclamationAdmin(admin.ModelAdmin):
+    list_display = ["text", "html_name"]
+
+
+@admin.register(AnswerToReclamation)
+class AnswerToReclamationAdmin(admin.ModelAdmin):
+    list_display = ["text"]
+
+
+class RawVersesAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Форма", {"fields": ["html_name", "title", "date_of_writing"]}),
+        ("Содержание", {"fields": ["text"]}),
+    ]
+    list_display = ["title", "html_name", "date_of_writing"]
+    search_fields = ["text", "title", "html_name"]
+
+
+class EuProAdmin(admin.ModelAdmin):
+    list_display = ["title", "html_name", "date_of_writing", "text"]
+    search_fields = ["title", "title", "html_name"]
+
+
+class HermeneuticsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Форма", {"fields": ["html_name", "title", "raw_verses", "date_of_writing"]}),
+        ("Содержание", {"fields": ["text"]}),
+    ]
+    list_display = ["title", "raw_verses", "html_name", "date_of_writing"]
+    search_fields = ["text", "title", "html_name"]
+
+
+admin.site.register(RawVerses, RawVersesAdmin)
+admin.site.register(EuPro, EuProAdmin)
+admin.site.register(Hermeneutics, HermeneuticsAdmin)
+admin.site.register(Audio)
--- backend/avantgarde/models.py
+++ backend/avantgarde/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+
+class Reclamation(models.Model):
+    text = models.TextField(default="")
+    html_name = models.SlugField(blank=True, null=True, unique=True)
+
+    def __str__(self):
+        return f"{self.text[: 30]} - {self.html_name}"
+
+
+class AnswerToReclamation(models.Model):
+    text = models.TextField(default="")
+    reclamation = models.ForeignKey(
+        "Reclamation", on_delete=models.PROTECT, null=True, blank=True
+    )
+    repeat = models.PositiveIntegerField(default=100)
+
+    def __str__(self):
+        return f"{self.text[: 30]}"
+
+
+class RawVerses(models.Model):
+    html_name = models.CharField(max_length=50)
+    title = models.TextField(null=True)
+    text = models.TextField(max_length=50000, null=True)
+    date_of_writing = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        managed = True
+        db_table = "raw_verses"
+
+
+class Hermeneutics(models.Model):
+    raw_verses = models.ForeignKey(
+        RawVerses, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    html_name = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    text = models.TextField(max_length=65535)
+    date_of_writing = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        managed = True
+        db_table = "hermeneutics"
+
+
+class Audio(models.Model):
+    raw_verses = models.ForeignKey(
+        RawVerses, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    html_name = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    audio = models.FileField(upload_to="audio/", null=True, blank=True)
+    date_of_writing = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        managed = True
+        db_table = "audio"
+
+
+class EuPro(models.Model):
+    html_name = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    text = models.TextField(max_length=65535)
+    date_of_writing = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.pk) + ". " + self.title
+
+    class Meta:
+        managed = True
+        db_table = "eu_pro"
